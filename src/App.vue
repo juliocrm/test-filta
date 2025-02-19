1ca5f772b92e5cdebeb677930866cab41c0500e9
julcrm <julcrm@gmail.com>
2025-02-19 08:04:51 +0000

    Fix Tailwind installation - add base landingPage file

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import LandingPage from './LandingPage.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app" class="w-full">
+    <div class="w-full md:w-[80%] mx-auto">
+      <LandingPage/>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ src/LandingPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <title></title>
+        <section class="container mx-auto py-12">
+            <div class="md:flex md:items-center md:space-x-8">
+                <div class="md:w-1/2">
+                    <img src="https://media.istockphoto.com/id/1343574927/photo/landscape-near-elbow-in-rh%C3%B6n.jpg?s=612x612&w=0&k=20&c=zgYlNBVufruUcWJvF5CNEvFZTWIdsZihLaaNPBpVIYs=" alt="Test image description" class="w-full h-auto rounded-lg shadow-md">
+                </div>
+                <div class="md:w-1/2">
+                    <p class="text-gray-700">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    </p>
+                </div>
+            </div>
+        </section>
+
+    </div>
+</template>
