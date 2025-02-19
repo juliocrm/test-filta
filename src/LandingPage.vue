de4544d77087f9b8a814d4d95be26f3aa7dd4d3b
julcrm <julcrm@gmail.com>
2025-02-19 08:55:48 +0000

    Improve style for main image and open its modal

--- src/LandingPage.vue
+++ src/LandingPage.vue
@@ -4,14 +4,19 @@
         <title></title>
         <section class="container mx-auto py-12">
             <div class="md:flex md:items-center md:space-x-8">
-                <div class="md:w-1/2">
-                    <img src="https://media.istockphoto.com/id/1343574927/photo/landscape-near-elbow-in-rh%C3%B6n.jpg?s=612x612&w=0&k=20&c=zgYlNBVufruUcWJvF5CNEvFZTWIdsZihLaaNPBpVIYs=" alt="Test image description" class="w-full h-auto rounded-lg shadow-md">
-                </div>
-                <div class="md:w-1/2">
-                    <p class="text-gray-700">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    </p>
-                </div>
+            <div class="md:w-1/2 flex justify-center">
+                <img
+                src="https://i.pinimg.com/736x/dd/0b/0d/dd0b0d507240b89f23ff6c0157ac03d0.jpg"
+                alt="Test image description"
+                class="max-w-full max-h-[80vh] rounded-lg shadow-md cursor-pointer"
+                @click="openModal('https://i.pinimg.com/736x/dd/0b/0d/dd0b0d507240b89f23ff6c0157ac03d0.jpg')"
+                />
+            </div>
+            <div class="md:w-1/2">
+                <p class="text-gray-700">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+            </div>
             </div>
         </section>
 
