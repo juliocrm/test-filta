029507fdc1d79142aa5a08b07a12e1b7c176a090
julcrm <julcrm@gmail.com>
2025-02-19 08:29:36 +0000

    Add image modal and improve images style

--- src/LandingPage.vue
+++ src/LandingPage.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <ImageModal :isOpen="isModalOpen" :imageSrc="currentImage" @close="closeModal" />
         <title></title>
         <section class="container mx-auto py-12">
             <div class="md:flex md:items-center md:space-x-8">
@@ -14,5 +15,44 @@
             </div>
         </section>
 
+        <section class="container mx-auto px-4 py-8">
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <div v-for="image in images" :key="image.id" class="flex flex-col items-center">
+                <div class="w-full aspect-[9/16] overflow-hidden">
+                    <img :src="image.url" :alt="image.alt" class="w-full h-full object-cover cursor-pointer" @click="openModal(image.url)" />
+                </div>
+                <p class="mt-2 text-center">{{ image.description }}</p>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
+
+<script>
+import ImageModal from './components/Modal.vue'
+export default {
+  name: 'LandingPage',
+  components: { ImageModal },
+  data() {
+    return {
+      images: [
+        { id: 1, url: 'https://i.pinimg.com/736x/b4/4e/87/b44e87858cc70b06c0064c4c9c64a2db.jpg', alt: 'Image 1', description: 'Description for image 1' },
+        { id: 2, url: 'https://i.pinimg.com/736x/9a/39/22/9a392291887ea4113e48966b929a1ad3.jpg', alt: 'Image 2', description: 'Description for image 2' },
+        { id: 3, url: 'https://i.pinimg.com/736x/93/9e/a8/939ea8de0c247c04fb5344873ab19301.jpg', alt: 'Image 3', description: 'Description for image 3' }
+      ],
+      isModalOpen: false,
+      currentImage: ''
+    }
+  },
+  methods: {
+    openModal(url) {
+      this.currentImage = url
+      this.isModalOpen = true
+    },
+    closeModal() {
+      this.isModalOpen = false
+      this.currentImage = ''
+    }
+  }
+}
+</script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-if="isOpen" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50" @click.self="handleClose">
+      <div class="relative">
+        <img :src="imageSrc" alt="Modal image" class="max-h-[80vh] w-auto object-contain" />
+        <button class="absolute top-2 right-2 text-white bg-gray-700 rounded-full p-2 hover:bg-gray-900" @click="handleClose">✕</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ImageModal',
+    props: {
+      isOpen: { type: Boolean, required: true },
+      imageSrc: { type: String, default: '' }
+    },
+    methods: {
+      handleClose() {
+        this.$emit('close')
+      }
+    }
+  }
+  </script>
+  
