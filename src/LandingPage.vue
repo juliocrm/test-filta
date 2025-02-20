25bc6b0dc94487e9a23d8502a1126d5f331afa5d
julcrm <julcrm@gmail.com>
2025-02-20 02:59:32 +0000

    Improve sample content and fix images alignment

--- src/LandingPage.vue
+++ src/LandingPage.vue
@@ -18,7 +18,7 @@
           />
         </div>
         <div class="md:w-1/2">
-          <p class="text-gray-700 mt-4" v-if="landingPage.heroImage">
+          <p class="text-gray-700 mt-4 md:mt-0 font-semibold" v-if="landingPage.heroImage">
             {{ landingPage.heroImage.description }}
           </p>
         </div>
@@ -26,15 +26,15 @@
     </section>
 
     <section class="container mx-auto py-14 h-screen">
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6 h-full items-center justify-center">
-        <div v-for="image in images" :key="image.id" class="flex flex-col items-center">
-          <div class="w-full max-w-[240px] aspect-[9/16] overflow-hidden">
-            <img :src="image.url" :alt="image.alt" class="w-full h-full object-cover cursor-pointer" @click="openModal(image.url)" />
-          </div>
-          <p class="mt-2 text-center">{{ image.description }}</p>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6 h-full items-start justify-center">
+      <div v-for="image in images" :key="image.id" class="flex flex-col h-full items-start">
+        <div class="w-full max-w-[240px] aspect-[9/16] overflow-hidden flex-none mx-auto">
+          <img :src="image.url" :alt="image.alt" class="w-full h-full object-cover cursor-pointer" @click="openModal(image.url)" />
         </div>
+        <p class="mt-2 text-center w-full">{{ image.description }}</p>
       </div>
-    </section>
+    </div>
+  </section>
 
   </div>
 </template>
