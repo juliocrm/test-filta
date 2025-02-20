0bcaa71a540074e423463ed295a50b8d69767fc6
julcrm <julcrm@gmail.com>
2025-02-20 05:22:43 +0000

    Enable mirage for production

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import './style.css'
 import App from './App.vue'
 import { makeServer } from "./mirage/server";
 
-if (process.env.NODE_ENV === "development") {
-  makeServer();
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === "production" || process.env.MIRAGE_ENABLED === 'true') {
+  makeServer({ environment: process.env.NODE_ENV });
 }
 
 createApp(App).mount('#app')
